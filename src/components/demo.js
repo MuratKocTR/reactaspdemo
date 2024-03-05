f479806d820e335ead5f706d31162f620465a0eb
Murat KOÇ <45242131+MuratKocTR@users.noreply.github.com>
2024-03-05 14:36:07 +0300

    Fazla Dosyalar silindi

--- src/components/demo.js
+++
@@ -1,9 +0,0 @@
-import DateTimePicker from "react-datetime-picker";
-function Demo(){
-    return(
-<div className="p-5">
-<input aria-label="Date and time" type="datetime-local" />
-</div>
-    );
-}
-export default Demo;
